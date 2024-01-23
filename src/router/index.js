426774860a0c1c36e1e1229a98b0999515a32eba
thienis94 <thierrynisengwe3@gmail.com>
2024-01-23 20:35:49 +0200

    changement bien

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from "../views/LoginView.vue"
+import ListeEmlpoyer from "../views/ListeEmlpoyer.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +10,11 @@ const router = createRouter({
       name: 'LOginView',
       component: LoginView
     },
-   
+  {
+    path: '/ListeEmlpoyer',
+    name: 'ListeEmlpoyer',
+    component: ListeEmlpoyer
+  }
   ]
 })
 
---
+++ src/views/ListeEmployer.vue
@@ -0,0 +1,11 @@
+
+<template>
+   <div> les hommes</div>
+</template>
+<script >
+
+</script>
+
+<style scoped>
+
+</style>
